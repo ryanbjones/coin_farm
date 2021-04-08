7b523154e979c631f2ea06248ec9d10c75768bec
Ryan Jones <run0391@gmail.com>
2021-04-07 23:53:38 -0600

    Refactor to simplify

--- lib/twitter/pokemon_retriever.ex
+++ lib/twitter/pokemon_retriever.ex
@@ -3,41 +3,30 @@ defmodule Twitter.PokemonRetriever do
 
   @pokemon_url "https://pokeapi.co/api/v2/pokemon"
 
-  def retrieve(identifier) do
+  def identifier_to_return(identifier) do
     case Integer.parse(identifier) do
-      :error ->
-        Logger.info("identifier is a string: #{identifier}")
-        find_id_by_name(identifier)
-      {identifier, _} ->
-        Logger.info("identifier is an integer #{identifier}")
-        find_name_by_id(identifier)
+      :error -> :integer
+      {_int, ""} -> :name
     end
   end
 
-  def find_id_by_name(name) do
-    parsed_response =
-      case fetch(name) do
-        {:ok, %{status_code: 200} = response} ->
-          {:ok, %{"id" => id}} = Jason.decode(response.body)
-          id
-        {:ok, %{status_code: 404}} ->
-          "Not found"
-        unknown_response ->
-          Logger.error("Unknown reponse from pokemon: #{inspect(unknown_response)}")
-      end
+  def retrieve(identifier) do
+    case identifier_to_return(identifier) do
+      :name -> find_pokemon(identifier, "name")
+      :integer -> find_pokemon(identifier, "id")
+    end
   end
 
-  def find_name_by_id(id) do
-    parsed_response =
-      case fetch(id) do
-        {:ok, %{status_code: 200} = response} ->
-          {:ok, %{"name" => name}} = Jason.decode(response.body)
-          name
-        {:ok, %{status_code: 404}} ->
-          "Not found"
-        unknown_response ->
-          Logger.error("Unknown reponse from pokemon: #{inspect(unknown_response)}")
-      end
+  def find_pokemon(name, returning_attribute) do
+    case fetch(name) do
+      {:ok, %{status_code: 200} = response} ->
+        {:ok, %{^returning_attribute => attr}} = Jason.decode(response.body)
+        attr
+      {:ok, %{status_code: 404}} ->
+        "Not found"
+      unknown_response ->
+        Logger.error("Unknown reponse from pokemon: #{inspect(unknown_response)}")
+    end
   end
 
   defp fetch(identifier) do
